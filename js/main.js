41bc1b8e2e64a48b9d67b5769c6fe790c65f1477
Nahuel Escobar <e.nahuelescobar@gmail.com>
2023-02-27 19:10:02 -0300

    Agregue animacion al menu

--- js/main.js
+++ js/main.js
@@ -5,7 +5,10 @@ let bars = document.querySelector(".bars")
 
 // Le doy el evento "click" a bars
 bars.addEventListener("click", ()=>{
+    // Agarro la clase para que el nav aparezca
     let navBar = document.querySelector(".nav-bar")
     navBar.classList.toggle("active")
+    // Le agrego una clase al bars para animacion
+    bars.classList.toggle("open")
 })
 
