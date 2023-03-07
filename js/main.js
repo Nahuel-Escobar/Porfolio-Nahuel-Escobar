bfdaba381c32136ee00aa88e12e10550f41ed6e2
Nahuel Escobar <e.nahuelescobar@gmail.com>
2023-03-07 17:08:11 -0300

    Agrego pdf y elimino sweetalert

--- js/main.js
+++ js/main.js
@@ -10,21 +10,3 @@ bars.addEventListener("click", ()=>{
     // Le agrego una clase al bars para animacion
     bars.classList.toggle("open")
 })
-
-
-// Creo una alerta para el boton resumen. Diciendo que no esta habilitado
-
-const resumen = document.getElementById("resumen")
-
-resumen.addEventListener("click", ()=>{
-    Swal.fire({
-        title: 'Cv no encontrado',
-        text: 'Actualmente me encuentro creando mi cv en pdf, disculpe las molestias',
-        imageUrl: 'https://2.bp.blogspot.com/-YogAEK4VD4c/WOtX_gF7XSI/AAAAAAAADhk/1PVsOjqfk1k5Cxzm6nS150CL_Tg6NDdPgCLcB/s400/mossfire0ph.gif',
-        imageWidth: 300,
-        imageHeight: 200,
-        imageAlt: 'problemas tecnicos',
-        width: 400,
-        
-      })
-})
